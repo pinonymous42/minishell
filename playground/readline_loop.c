bc5fabc212717682dd37f49d26fc01d6d4b63438
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-27 22:09:11 +0900

    commit

--- playground/readline_loop.c
+++ playground/readline_loop.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 19:28:13 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/01/27 19:32:35 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/01/27 22:02:30 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,22 @@ int main(void)
             break;
         if (*line)
             add_history(line);
+        printf("%s", line);
         free(line);
     }
     exit(0);
 }
+
+/*
+実行結果①
+minishell$aaa
+minishell$aaa
+minishell$aaa
+minishell$aaaaaaaa
+minishell$123445
+minishell$123445
+minishell$
+minishell$
+minishell$
+minishell$^Daaaaaaaaaaaaaaaaa123445123445bash-3.2$ 
+*/
