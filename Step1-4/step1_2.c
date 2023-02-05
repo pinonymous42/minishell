2620a733d4037ebc03e0b5fb258edb6c0d771023
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-02-06 00:08:27 +0900

    commit

--- Step1-4/step1_2.c
+++ Step1-4/step1_2.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 01:39:22 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/02/05 23:01:01 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/02/05 23:05:32 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@ int main(void)
 {
     char *line;
     
+    /*
+    rl_outstream を使ってreadlineの出力先をstderrに変更しました。
+    macの標準で入っているreadlineはデフォルトで出力先がstderrなのでこれは必要ないのですが、
+    GNUのreadlineは初期設定がstdoutになっているためです。
+    これを標準出力のままにしておくと、テストで標準出力のdiffを比較する際にreadlineのpromptが出力されてしまって困るので、
+    このように標準エラー出力に吐き出すように設定しています。
+    */
     rl_outstream = stderr;
     while (1)
     {
