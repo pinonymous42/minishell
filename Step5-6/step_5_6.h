cafce1ba9efd0aa2087e8e077b124becc1394c74
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-02-06 20:18:13 +0900

    commit

--- Step5-6/step_5_6.h
+++ Step5-6/step_5_6.h
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 16:18:59 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/02/06 17:09:37 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/02/06 17:17:58 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,17 @@
 
 # include "step_1_4.h"
 
+typedef enum e_token_kind{
+    TK_WORD,
+	TK_RESERVED,
+	TK_OP,
+	TK_EOF,
+}t_token_kind;
 
+typedef struct s_token{
+    t_token_kind    kind;
+    char            *word;
+    t_token         *next;
+}t_token;
 
 #endif
