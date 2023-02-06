1e596c3411fe5420e235245f8915c912038a3afc
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-02-06 16:22:44 +0900

    commit

---
+++ Step5-6/step5_6.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   step5_6.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 16:18:59 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/02/06 16:19:12 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <limits.h>
+
+# include <string.h>
+
+#endif
