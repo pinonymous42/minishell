df3fc8c462f073696819d6325efd406f2802f336
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-02-08 21:16:37 +0900

    commit

--- Step5-6/includes/step_5_6.h
+++ Step5-6/includes/step_5_6.h
@@ -6,13 +6,14 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 16:18:59 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/02/06 17:17:58 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/02/08 20:03:58 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STEP_5_6_H
 # define STEP_5_6_H
 
+# include <stdbool.h>
 # include "step_1_4.h"
 
 typedef enum e_token_kind{
