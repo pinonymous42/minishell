54f2ebb50c700d272e87b9f636a152e9272e476a
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-02-23 14:01:21 +0900

    commit

---
+++ Step9-10-refuctering/Part6_pipe_make_exe_path_ref/srcs/convert_token_list_to_array.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convert_token_list_to_array.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 16:12:28 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/02/22 19:13:45 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+//refuctering必要
+
+char	**tail_recursive(t_token *token, int nargs, char **array)
+{
+	if (token == NULL || token->kind == TOKEN_EOF)
+		return (array);
+	array = reallocf(array, (nargs + 2) * sizeof(char *)); //修正必要
+	array[nargs] = ft_strdup(token->word);
+	if (array[nargs] == NULL)
+		function_error("strdup");
+	array[nargs + 1] = NULL;
+	return (tail_recursive(token->next, nargs + 1, array));
+}
+
+char	**token_list_to_array(t_token *token)
+{
+	char	**array;
+
+	array = ft_calloc(1, sizeof(char *));
+	if (array == NULL)
+		function_error("calloc");
+	return (tail_recursive(token, 0, array));
+}
