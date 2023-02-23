7a4a2c24b26dd5b68a2f60324cbee97e554f885d
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-02-23 18:52:16 +0900

    commit

---
+++ Step9-10-refuctering/Part8_ref_gl_syn/srcs/error_ref.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_ref.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 15:33:36 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/02/23 18:46:01 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+bool	syntax_error = false;
+
+void    function_error(char *function_name)
+{
+    printf("function error: %s\n", function_name);
+    exit(EXIT_FAILURE);
+}
+
+void    tokenize_error(char *message, char **rest, char *line, bool *syntax_error)
+{
+    syntax_error = true;
+    printf("minishell: syntax error near %s\n", message);
+    while (*line)
+		line++;
+	*rest = line;
+}
+
+void	assert_error(const char *msg)
+{
+	dprintf(STDERR_FILENO, "Assert Error: %s\n", msg);
+	exit(255);
+}
+
+void	err_exit(const char *location, const char *msg, int status)
+{
+	dprintf(STDERR_FILENO, "minishell: %s: %s\n", location, msg);
+	exit(status);
+}
