2d16367e7ea546be82fac21ed1128410f108beb8
pinonymous <matsu1088@icloud.com>
2023-03-14 20:29:47 +0900

    kohmatsu_norminette

---
+++ kohmatsu_norminette/srcs/5_1_signal.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   5_1_signal.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kohmatsu <kohmatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/26 12:54:49 by kohmatsu          #+#    #+#             */
+/*   Updated: 2023/03/14 19:45:29 by kohmatsu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	signal_handler(int sig)
+{
+	(void)sig;
+	g_signal.status = 1;
+	g_signal.other_code = TRUE;
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	write(1, "\n", 1);
+	rl_redisplay();
+}
+
+int	set_signal(void)
+{
+	signal(SIGINT, &signal_handler);
+	signal(SIGQUIT, SIG_IGN);
+	return (0);
+}
+
+int	set_signal_child(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	return (0);
+}
+
+int	set_signal_parent(void)
+{
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	return (0);
+}
---
+++ kohmatsu_norminette/srcs/8_2_error.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   8_2_error.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 18:42:10 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/03/13 18:43:45 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	assert_error(const char *msg)
+{
+	my_dprintf(STDERR_FILENO, "Assert Error: %s\n", msg);
+	exit(255);
+}
+
+void	err_exit(const char *location, const char *msg)
+{
+	my_dprintf(STDERR_FILENO, "minishell: %s: %s\n", location, msg);
+	exit(127);
+}
+
+void	command_not_found(const char *location)
+{
+	my_dprintf(STDERR_FILENO, "minishell: %s: command not found\n", location);
+	exit(127);
+}
+
+void	file_not_found(const char *filename)
+{
+	my_dprintf(STDERR_FILENO, "minishell: %s: \
+		No such file or directory\n", filename);
+}
+
+void	ambiguous_redirect(char *filename)
+{
+	my_dprintf(STDERR_FILENO, "minishell: %s: ambiguous redirect\n", filename);
+}
