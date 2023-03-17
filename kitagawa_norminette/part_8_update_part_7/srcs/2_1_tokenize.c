ea9986834e9fa152426512bd607dc6e3bc389e83
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-03-17 13:49:51 +0900

    commit

---
+++ kitagawa_norminette/part_8_update_part_7/srcs/2_1_tokenize.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   2_1_tokenize.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 15:03:28 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/03/14 23:57:38 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	token_handle(t_token **head, char **rest, char *line)
+{
+	t_token	*token;
+
+	if (is_metacharacter(*line) == true)
+	{
+		token = handle_metacharacter(rest, line);
+		append_token(head, token);
+	}
+	else if (is_control_operator(line) == true)
+	{
+		token = handle_contorol_op(rest, line);
+		append_token(head, token);
+	}
+	else
+	{
+		token = handle_word(rest, line);
+		append_token(head, token);
+	}
+}
+
+t_token	*tokenize(char *line)
+{
+	t_token	*head;
+
+	head = NULL;
+	while (*line != '\0')
+	{
+		if (is_blank(*line) == true)
+		{
+			while (is_blank(*line) == true)
+				line++;
+		}
+		token_handle(&head, &line, line);
+	}
+	check_and_sort_tokens(&head);
+	append_token(&head, new_token(TOKEN_EOF));
+	return (head);
+}
---
+++ kitagawa_norminette/part_8_update_part_7/srcs/6_5_pwd_builtin.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   6_5_pwd_builtin.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/02 11:55:33 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/03/13 16:54:25 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	pwd_builtin(t_info *info)
+{
+	char	cwd[PATH_MAX];
+
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		printf("%s\n", cwd);
+	else
+	{
+		while (info->list != NULL)
+		{
+			if (ft_strncmp(info->list->key, "PWD", ft_strlen("PWD")) == 0)
+			{
+				printf("%s\n", info->list->value);
+				break ;
+			}
+			info->list = info->list->next;
+		}
+	}
+}
