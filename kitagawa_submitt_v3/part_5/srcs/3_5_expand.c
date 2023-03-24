e9abd10e7d463e2a25c8597cb172b73611d2cf64
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-03-24 15:41:11 +0900

    commit

---
+++ kitagawa_submitt_v3/part_5/srcs/3_5_expand.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   3_1_4_expand.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 19:18:27 by kohmatsu          #+#    #+#             */
+/*   Updated: 2023/03/21 16:10:32 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	norm_double_expansion(char **ret, t_environ *list, char *var)
+{
+	if (search_env(var, list))
+		*ret = ft_strjoin_with_free(*ret, search_env(var, list), FIRST_PARAM);
+	else if (ft_isdigit(*var))
+	{
+		*ret = ft_strjoin_with_free(*ret, (var + 1), FIRST_PARAM);
+		g_signal.not_expand_flag = 1;
+	}
+}
+
+void	double_expansion(char **new_word, char **ret, t_environ *list)
+{
+	char	*var;
+
+	var = ft_calloc(sizeof(char), 1);
+	if (ft_strchr(*new_word, '$') || ft_strchr(*new_word, ' ')
+		|| not_allowed_variant_character(*new_word))
+	{
+		while (**new_word != '$' && **new_word != ' ' \
+			&& is_variable_character(**new_word))
+		{
+			append_char(&var, **new_word);
+			*new_word += 1;
+		}
+	}
+	else
+	{
+		free(var);
+		var = ft_strndup(*new_word, ft_strchr(*new_word, '\0') - *new_word);
+	}
+	norm_double_expansion(ret, list, var);
+	free(var);
+}
+
+void	double_moving(char **new_word)
+{
+	if (ft_strchr(*new_word, '$') || ft_strchr(*new_word, ' '))
+	{
+		if (ft_strchr(*new_word, ' ') == 0 || (ft_strchr(*new_word, '$')
+				&& (ft_strchr(*new_word, '$') < ft_strchr(*new_word, ' '))))
+			*new_word = ft_strchr(*new_word, '$');
+		else if (ft_strchr(*new_word, '$') == 0 || (ft_strchr(*new_word, ' ')
+				&& (ft_strchr(*new_word, '$') > ft_strchr(*new_word, ' '))))
+			*new_word = ft_strchr(*new_word, ' ');
+	}
+	else
+	{
+		if (not_allowed_variant_character(*new_word))
+		{
+			while (is_variable_character(**new_word))
+				*new_word += 1;
+		}
+		else
+			*new_word = ft_strchr(*new_word, '\0');
+	}
+}
+
+void	double_expand_status(char **ret, char **new_word)
+{
+	*ret = ft_strjoin_with_free(*ret, ft_itoa(g_signal.status), FIRST_PARAM);
+	*new_word += 1;
+}
+
+char	*double_variable_expand(char *new_word, t_environ *list)
+{
+	char	*tmp;
+	char	*ret;
+
+	g_signal.do_split = 1;
+	tmp = new_word;
+	while (*new_word != '$')
+		new_word++;
+	if (new_word == tmp)
+		ret = ft_calloc(sizeof(char), 1);
+	else
+		ret = ft_strndup(tmp, new_word - tmp);
+	new_word++;
+	if (*new_word == '?')
+		double_expand_status(&ret, &new_word);
+	else
+	{
+		double_expansion(&new_word, &ret, list);
+		double_moving(&new_word);
+	}
+	ret = ft_strjoin_with_free(ret, new_word, FIRST_PARAM);
+	free(tmp);
+	return (ret);
+}
---
+++ kitagawa_submitt_v3/part_5/srcs/6_6_1_export_builtin.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   6_6_1_export_builtin.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 20:28:47 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/03/20 16:16:11 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	put_env(t_environ *list)
+{
+	t_environ	*tmp;
+
+	tmp = list;
+	while (tmp != NULL)
+	{
+		ft_putstr_fd("declare -x ", 1);
+		ft_putstr_fd(tmp->key, 1);
+		if (ft_strcmp(tmp->value, "\\") != 0)
+		{
+			ft_putstr_fd("=\"", 1);
+			if (tmp->value != NULL)
+				ft_putstr_fd(tmp->value, 1);
+			ft_putstr_fd("\"", 1);
+		}
+		ft_putstr_fd("\n", 1);
+		tmp = tmp->next;
+	}
+}
+
+char	*make_key(char *argv)
+{
+	char	*key;
+
+	if (ft_strchr_index(argv, '=') != -1)
+		key = ft_strndup(argv, ft_strchr_index(argv, '='));
+	else
+		key = ft_strdup(argv);
+	return (key);
+}
+
+bool	judge_key(char *key, char *argv, t_environ *list)
+{
+	if (not_allowed_variant_character(key) || ft_isdigit(key[0]) == 1)
+		export_not_valid_identifier(argv);
+	if (ft_strchr(key, '$') != NULL)
+		return (false);
+	else if (search_env(key, list) != NULL)
+		update_env(argv, list);
+	else if (search_env(key, list) == NULL
+		&& !not_allowed_variant_character(key))
+		add_new_env(argv, list);
+	return (true);
+}
+
+void	handle_argv(t_info *info, t_environ *list)
+{
+	int		i;
+	char	*key;
+
+	i = 1;
+	while (info->argv[i] != NULL)
+	{
+		if (check_add_or_not(info->argv[i], list) == true)
+			add_env_value(info->argv[i], list);
+		else
+		{
+			key = make_key(info->argv[i]);
+			if (judge_key(key, info->argv[i], list) == false)
+			{
+				if (yatto_owari(&key, &i))
+					continue ;
+				break ;
+			}
+			free(key);
+		}
+		i++;
+	}
+}
+
+void	export_builtin(t_info *info, t_environ *list)
+{
+	if ((info->argv[1] == NULL && info->argv_count == 2)
+		|| check_argv_no_such_env(info) == true)
+		put_env(list);
+	else
+		handle_argv(info, list);
+	return ;
+}
