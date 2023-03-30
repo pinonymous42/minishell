34fdb3ee6e947395ac0f15e8285a3e7952f64fa2
pinonymous <matsu1088@icloud.com>
2023-03-30 15:21:59 +0900

    kohmatsu_recover

--- kohmatsu_submit_latest/srcs/4_1_pipe.c
+++ kohmatsu_submit_latest/srcs/4_1_pipe.c
@@ -6,7 +6,7 @@
 /*   By: kohmatsu <kohmatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 23:07:46 by kohmatsu          #+#    #+#             */
-/*   Updated: 2023/03/30 14:24:48 by kohmatsu         ###   ########.fr       */
+/*   Updated: 2023/03/30 15:13:26 by kohmatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,7 @@ void	finish(t_info *info)
 	close(info->input_fd);
 	close(0);
 	dup2(g_signal.input_fd, 0);
+	dup2(g_signal.output_fd, 1);
 }
 
 void	pipex(int argc, char **argv, t_environ **list)
