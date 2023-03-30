be28a13c999ac337ae3d07beb08ff9ff70fb425f
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-03-30 14:44:40 +0900

    commit

---
+++ submit_v4_3_30/srcs/6_4_2_cd_builtin.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   6_4_2_cd_builtin.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 16:29:30 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/03/30 02:04:55 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	norm_set_old_and_new_pwd(char *new_pwd_dup, t_environ *list)
+{
+	t_environ	*tmp;
+
+	tmp = list;
+	while (tmp != NULL)
+	{
+		if (ft_strcmp(tmp->key, "PWD") == 0)
+		{
+			free(tmp->value);
+			tmp->value = new_pwd_dup;
+			break ;
+		}
+		tmp = tmp->next;
+	}
+	if (tmp == NULL)
+		free(new_pwd_dup);
+}
+
+void	set_old_and_new_pwd(t_environ *list, char *new_pwd_dup)
+{
+	t_environ	*tmp;
+
+	tmp = list;
+	while (tmp != NULL)
+	{
+		if (ft_strcmp(tmp->key, "OLDPWD") == 0)
+		{
+			if (ft_strcmp(tmp->value, "") == 0 && \
+				search_env("PWD", list) == NULL)
+			{
+				free(tmp->value);
+				tmp->value = new_pwd_dup;
+				return ;
+			}
+			else
+				free(tmp->value);
+			if (search_env("PWD", list) == NULL)
+				tmp->value = ft_strdup("");
+			else
+				tmp->value = ft_strdup(search_env("PWD", list));
+			break ;
+		}
+		tmp = tmp->next;
+	}
+	norm_set_old_and_new_pwd(new_pwd_dup, list);
+}
---
+++ submit_v4_3_30/srcs/utils2.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kohmatsu <kohmatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 00:03:28 by kohmatsu          #+#    #+#             */
+/*   Updated: 2023/03/29 15:43:49 by kohmatsu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int	is_space(char *str)
+{
+	while (*str)
+	{
+		if (*str != ' ')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+void	move_space(t_info *info, int start, int *i, int *count)
+{
+	int	j;
+
+	while (info->cmd[start][*i])
+	{
+		if (info->cmd[start][*i] == ' ')
+		{
+			j = 0;
+			while (info->cmd[start][*i + j] == ' ')
+				j++;
+			if (info->cmd[start][*i + j])
+				*count += 1;
+			while (info->cmd[start][*i] == ' ')
+				*i += 1;
+		}
+		else
+			*i += 1;
+	}
+}
+
+void	cut_wstatus(int wstatus)
+{
+	if (wstatus == SIGINT)
+		write(1, "\n", 1);
+	else if (wstatus == SIGQUIT)
+		write(1, "QUIT: 3\n", 8);
+	if (wstatus == SIGINT || wstatus == SIGQUIT)
+	{
+		g_signal.status = 128 + wstatus;
+		g_signal.other_code = TRUE;
+	}
+	else
+	{
+		g_signal.status = WEXITSTATUS(wstatus);
+		g_signal.other_code = TRUE;
+	}
+}
